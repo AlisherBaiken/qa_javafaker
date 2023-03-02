07e9bac6f3ea0af7e46e31e36ac043e80196f44c
Alisher <alisherbaiken@gmail.com>
2023-03-02 21:37:48 +0600

    added attach, screenshot, logas

--- src/test/java/fakerTest/tests/TestBaseExtended.java
+++ src/test/java/fakerTest/tests/TestBaseExtended.java
@@ -2,7 +2,9 @@ package fakerTest.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -31,6 +33,13 @@ public class TestBaseExtended {
     @BeforeEach
     void addListener(){
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+//        Attach.addVideo();
     }
 
 }
